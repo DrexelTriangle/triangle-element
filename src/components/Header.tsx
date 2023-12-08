acbe3e702dc947ff021bf135eda539f72ddfdb9c
vish <38266782+humboldt123@users.noreply.github.com>
2023-12-08 17:52:08 -0500

    add tri-icon png

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
 import LogoImg from "../assets/logo-white.svg";
+import TriIcon from "../assets/tri-icon.png";
 
 export const Header = (_props: {}) => {
     return (
@@ -8,6 +9,7 @@ export const Header = (_props: {}) => {
 			</div>
 	
 			<div className="search-container">
+				/* todo: figure out how to search in react */
 				<form role="search" method="get" className="search-form" action="<?php echo get_site_url(); ?>">
 					<input id="searchbox-main" type="search" className="search-textbox" placeholder="Search..." value="" name="s"></input>
 				</form>
@@ -15,7 +17,7 @@ export const Header = (_props: {}) => {
 	
 			<div className="header-logo">
 				<div className="header-logo-desktop">
-					<a id="triangle-logo" href="<?php echo esc_url(home_url('/')); ?>" rel="home">
+					<a id="triangle-logo" href="/" rel="home">
 						<img src={LogoImg}></img>
 					</a>
 				</div>
@@ -24,28 +26,12 @@ export const Header = (_props: {}) => {
 	
 			<div className="header-logo-mobile">
 				<div className="img-container">
-					<a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><img id="logo-mobile" src="<?php echo get_template_directory_uri() . '//images/tri-icon.png'; ?>"></img></a>
+					<a href="/" rel="home"><img id="logo-mobile" src={TriIcon}></img></a>
 				</div>
 			</div>
 
 			<div id="nav-icon" className="header-hamburger-icon white"><span></span><span></span><span></span></div>
 
 		</header>
-		/**<div id="nav-main" className="header-nav-container">
-				<?php
-					if(has_nav_menu('main'))
-						wp_nav_menu(array('theme_location' => 'main', 'menu_class' => 'main'));
-					else
-						// Display error message if menu "main" has not been defined within WordPress
-						echo 'Menu "main" is not defined!</br>';
-						
-					
-					if(has_nav_menu('sub'))
-						wp_nav_menu(array('theme_location' => 'sub', 'menu_class' => 'sub'));
-					else
-						// Display error message if menu "sub" has not been defined within WordPress
-						echo 'Menu "sub" is not defined!</br>';
-				?>
-			</div>*/
 	);
 }
