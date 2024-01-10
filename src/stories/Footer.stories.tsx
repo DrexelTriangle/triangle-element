ece9dd494c208b63e06a248b006964bacd00b220
Zaryab <madscientist2854@gmail.com>
2024-01-10 13:42:21 -0500

    refactor footer to react style

--- src/stories/Footer.stories.tsx
+++ src/stories/Footer.stories.tsx
@@ -13,6 +13,4 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Index: Story = {
-  args: {menu: []}
-};
+export const Index: Story = {};
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,15 +5,20 @@ const colors = require('tailwindcss/colors')
 export default {
   content: ["./src/**/*.{html,js,ts,tsx}"],
   theme: {
-    colors:{
-      primary: '#055C9D',
-      secondary: {
-        100: '#B4DFFC',
-        200: '#73BBEE',
-        300: '#2479DF',
-        400: '#094886',
+    extend: {
+      colors:{
+        primary: '#055C9D',
+        secondary: {
+          100: '#B4DFFC',
+          200: '#73BBEE',
+          300: '#2479DF',
+          400: '#094886',
+        },
       },
-    },
+      fontFamily: {
+        heading: ['Roboto Slab', 'Helvetica', 'Arial', 'sans-serif']
+      }
+    }
   },
   plugins: [],
 }
