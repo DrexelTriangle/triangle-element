26c58db271e9fae15e9ba00e3c6ebf4c6b527954
Zaryab <madscientist2854@gmail.com>
2024-01-11 04:43:14 -0500

    center everything on main page

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import './App.css'
 import { Footer } from './components/Footer.tsx';
 import { Header } from './components/Header.tsx';
 import { HeaderFrontPage } from './components/HeaderFrontPage.tsx';
+import { Features } from './components/Features.tsx'
 import "../TriangleX/style.css"
 
 function App() {
   return <>
     <Header />
-    <HeaderFrontPage />
+    <div className="max-w-[1280px] w-[90%] m-auto">
+      <HeaderFrontPage />
+      <Features />
+    </div>
     <Footer menu={[]}/>
   </>
 }
