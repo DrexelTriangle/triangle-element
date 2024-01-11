c75c5362d23b308b9fcc3964b6934c6480bb5614
Zaryab <madscientist2854@gmail.com>
2024-01-11 04:29:52 -0500

    add refactored main article component

--- src/components/MainArticle.tsx
+++ src/components/MainArticle.tsx
@@ -12,11 +12,13 @@ export const MainArticle = (props: {
 		url: string
 	}
 }) => {
-    return (<div className="flex flex-col gap-3">
-		<a href={props.article.link}><img src={props.image.url}></img></a>
-		<a className="text-headline-large" href={props.article.link}>{props.article.title}</a>
-		<div className="frontpage-postinfo">Featured this week in {props.article.category}</div>
-		<div className="category-author">By {props.article.author}  | {props.article.date.toDateString()}</div>
-		<p>{props.article.preview}</p>
-	</div>);
+  return <div className="px-4 border-x border-gray-300 flex flex-col gap-3">
+		<a href={props.article.link}><img src={props.image.url} /></a>
+		<a className="text-black text-[42px] leading-tight font-title"
+			href={props.article.link}>{props.article.title}</a>
+		<div className="text-xs font-heading">Featured this week in {props.article.category}</div>
+		<div className="text-slate-500 text-xs leading-none font-heading">
+			By {props.article.author}  | {props.article.date.toDateString()}</div>
+		<p className="line-clamp-2" dangerouslySetInnerHTML={{__html: props.article.preview}}></p>
+	</div>;
 }
