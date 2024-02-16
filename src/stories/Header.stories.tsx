e40f24453336966c1573bbe083576b386f154555
Zaryab <madscientist2854@gmail.com>
2024-02-16 08:13:58 -0500

    fix Header story

--- src/stories/Header.stories.tsx
+++ src/stories/Header.stories.tsx
@@ -1,10 +1,22 @@
 import type { Meta, StoryObj } from '@storybook/react';
+import { useState } from 'react';
 
 import { Header } from '../components/Header';
+import { HeaderMenu } from '../components/HeaderMenu';
+
+const Container = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [searchOpen, setSearchOpen] = useState(false);
+
+  return <div>
+    <Header menuOpen={menuOpen} setMenuOpen={setMenuOpen} searchOpen={searchOpen} setSearchOpen={setSearchOpen} />
+		<HeaderMenu open={menuOpen} setOpen={setMenuOpen} />
+  </div>
+}
 
 const meta = {
   title: 'Components/Header',
-  component: Header,
+  component: Container,
   parameters: {
     layout: 'fullscreen',
   },
